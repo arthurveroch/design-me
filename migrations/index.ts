95f21e5b7986f4cd6066db6d20223ef3e03db63f
arthurveroch <verhofstadtpro@gmail.com>
2026-05-07 16:31:12 +0200

    refactor: remove unused service components and associated configuration filesi

--- migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20260507_135411 from './20260507_135411';
-
-export const migrations = [
-  {
-    up: migration_20260507_135411.up,
-    down: migration_20260507_135411.down,
-    name: '20260507_135411'
-  },
-];
