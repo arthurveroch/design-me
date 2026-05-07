fcabda676435b46662e4f0a75ab235ebb6d8561e
arthurveroch <verhofstadtpro@gmail.com>
2026-05-07 16:42:54 +0200

    migration create

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260507_144244 from './20260507_144244';
+
+export const migrations = [
+  {
+    up: migration_20260507_144244.up,
+    down: migration_20260507_144244.down,
+    name: '20260507_144244'
+  },
+];
