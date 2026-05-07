84b568e14ba57ae3f82095f8f5842dcf5fa14595
arthurveroch <verhofstadtpro@gmail.com>
2026-05-07 14:43:10 +0200

    revalidate correction

--- src/app/(frontend)/career/page.tsx
+++ src/app/(frontend)/career/page.tsx
@@ -2,7 +2,7 @@ import FaqJob from '@/app/components/faq-job/FaqJob'
 import HeroCareer from '@/app/components/hero-career/HeroCareer'
 import Jobs from '@/app/components/jobs/Jobs'
 
-export const revalidate = 3600 * 24 * 7
+export const revalidate = 604800
 
 export default function CareerPage() {
   return (
