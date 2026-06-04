63c0ce484688ddfde8a84de4f48e7a7929deabc4
arthurveroch <verhofstadtpro@gmail.com>
2026-06-04 16:03:43 +0200

    feat: initialize Payload CMS with database migrations and base component styles

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260604_134147 from './20260604_134147';
+
+export const migrations = [
+  {
+    up: migration_20260604_134147.up,
+    down: migration_20260604_134147.down,
+    name: '20260604_134147'
+  },
+];
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
+import {migrations} from "./migrations"
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
@@ -35,6 +36,7 @@ export default buildConfig({
     pool: {
       connectionString: process.env.DATABASE_URL || '',
     },
+    prodMigrations : migrations
   }),
   sharp,
   plugins: [],
