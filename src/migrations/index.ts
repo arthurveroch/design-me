82ceb0d4adf369fc279cb420fa0bef274adf7c06
arthurveroch <verhofstadtpro@gmail.com>
2026-06-04 16:22:47 +0200

    Revert "feat: initialize Payload CMS with database migrations and base component styles"
    This reverts commit 63c0ce484688ddfde8a84de4f48e7a7929deabc4.

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20260604_134147 from './20260604_134147';
-
-export const migrations = [
-  {
-    up: migration_20260604_134147.up,
-    down: migration_20260604_134147.down,
-    name: '20260604_134147'
-  },
-];
